b59204f396f90a611be161c9a6fa1763d13f845e
CChevalier <cchevalier70@gmail.com>
2015-06-13 12:34:02 +0200

    First Layout Design and About implementation

--- app/src/main/java/net/cchevalier/adnd/myappportofolio/MainActivity.java
+++ app/src/main/java/net/cchevalier/adnd/myappportofolio/MainActivity.java
@@ -2,8 +2,11 @@ package net.cchevalier.adnd.myappportofolio;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -30,6 +33,10 @@ public class MainActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_about) {
+            Log.i("folio", "About was selected");
+            Toast about = Toast.makeText(getBaseContext(), "Portfolio App developed \nfor the Android Developer \nNanoDegree from Udacity. \n(c) CChevalier, 2015", Toast.LENGTH_LONG);
+            about.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+            about.show();
             return true;
         }
 
